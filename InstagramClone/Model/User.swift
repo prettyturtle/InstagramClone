7eaffdee6e995171dcbd01fbeede665271748d94
prettyturtle <unifconti@gmail.com>
2022-04-17 02:43:13 +0900

    fix: User 모델의 feed, follower, following의 타입을 [String]으로 변경, mockUser 추가

--- InstagramClone/Model/User.swift
+++ InstagramClone/Model/User.swift
@@ -16,10 +16,10 @@ struct User: Codable, Equatable {
     let name: String
     let nickName: String
     let profileImageURLString: String
-    let feed: [Feed]
-    let follower: [User]
-    let following: [User]
-    let like: [Feed]
+    let feed: [String]
+    let follower: [String]
+    let following: [String]
+    let like: [String]
     
     var profileImageURL: URL? { URL(string: profileImageURLString) }
     
@@ -28,6 +28,26 @@ struct User: Codable, Equatable {
         name: "이영찬",
         nickName: "20._.chan",
         profileImageURLString: "https://i.picsum.photos/id/503/2560/1440.jpg?hmac=IdEK6g8OmWZiHZYfiwldM9qYPcd8PpBSSSj-mYoegkU",
+        feed: [],
+        follower: [],
+        following: [],
+        like: []
+    )
+    static let mockUser2 = User(
+        id: "3608CA92-C13D-4DD5-9AF0-E6CD8DAB6930",
+        name: "김승헌",
+        nickName: "mult_chord.sh",
+        profileImageURLString: "https://i.picsum.photos/id/989/2560/1440.jpg?hmac=piUBx_I3dNIviu-fAG96S-SuXDRVqdf8Q2-rJvR4CjM",
+        feed: [],
+        follower: [],
+        following: [],
+        like: []
+    )
+    static let mockUser3 = User(
+        id: "95749E80-3515-4964-9AA2-705852FB1776",
+        name: "정민규",
+        nickName: "min._._.gyu",
+        profileImageURLString: "https://i.picsum.photos/id/551/2560/1440.jpg?hmac=1rnt3o8xNDJvEf_jaFv21Yo3A2EvxlCFBlL_PIkXpZU",
         feed: [],
         follower: [],
         following: [],
